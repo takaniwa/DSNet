4e3ceaea1f39290ff837962dbf8707b2f7aee1f8
takaniwa <52619499+takaniwa@users.noreply.github.com>
2024-04-26 12:12:45 +0800

    Create __init__.py

---
+++ datasets/__init__.py
@@ -0,0 +1,10 @@
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from .cityscapes import Cityscapes as cityscapes
+from .camvid import CamVid as camvid
+from .mapillary import Mapillary as mapillary
+from .bdd10k import BDD as bdd
+from .ade20k import ADE20K as ade20k
