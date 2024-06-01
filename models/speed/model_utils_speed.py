7bafc561292494a7bd409208e9bf6ff0b1f7a53d
takaniwa <52619499+takaniwa@users.noreply.github.com>
2024-06-01 16:36:50 +0800

    Update model_utils_speed.py

--- models/speed/model_utils_speed.py
+++ models/speed/model_utils_speed.py
@@ -1,6 +1,4 @@
-# ------------------------------------------------------------------------------
-# Written by Jiacong Xu (jiacong.xu@tamu.edu)
-# ------------------------------------------------------------------------------
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -265,32 +263,6 @@ class segmenthead_drop(nn.Module):
         x = self.conv1(self.relu(self.bn1(x)))
         x = self.drop(self.relu(self.bn2(x)))
         out = self.conv2(x)
-        if self.scale_factor is not None:
-            height = x.shape[-2] * self.scale_factor
-            width = x.shape[-1] * self.scale_factor
-            out = F.interpolate(out,
-                        size=[height, width],
-                        mode='bilinear', align_corners=algc)
-
-        return out
-
-class segmenthead_l(nn.Module):
-
-    def __init__(self, inplanes, interplanes, outplanes, scale_factor=None):
-        super(segmenthead, self).__init__()
-        self.bn1 = BatchNorm2d(inplanes, momentum=bn_mom)
-        self.conv1 = nn.Conv2d(inplanes, interplanes, kernel_size=3, padding=1, bias=False)
-        self.bn2 = BatchNorm2d(interplanes, momentum=bn_mom)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv2d(interplanes, outplanes, kernel_size=1, padding=0, bias=True)
-        self.scale_factor = scale_factor
-        self.bn3 = BatchNorm2d(outplanes, momentum=bn_mom)
-
-    def forward(self, x):
-        
-        x = self.conv1(self.relu(self.bn1(x)))
-        out = self.conv2(self.relu(self.bn2(x)))
-        out = self.bn3(out)
         if self.scale_factor is not None:
             height = x.shape[-2] * self.scale_factor
             width = x.shape[-1] * self.scale_factor
