214b7847285abcc570e0eb5ddc5dafbfd740730c
takaniwa <52619499+takaniwa@users.noreply.github.com>
2024-06-16 21:17:42 +0800

    Update bdd10k.py

--- datasets/bdd10k.py
+++ datasets/bdd10k.py
@@ -1,189 +1,106 @@
-
-import cv2
+import os
 import numpy as np
-import random
+from PIL import Image
 
-from torch.nn import functional as F
-from torch.utils import data
+from .base_dataset import BaseDataset
 
-y_k_size = 6
-x_k_size = 6
-
-class BaseDataset(data.Dataset):
-    def __init__(self,
-                 ignore_label=255,
-                 base_size=2048,
-                 crop_size=(512, 1024),
+class BDD(BaseDataset):
+    def __init__(self, 
+                 root, 
+                 list_path, 
+                 num_classes=19,
+                 multi_scale=True, 
+                 flip=True, 
+                 ignore_label=255, 
+                 base_size=1280, 
+                 crop_size=(1280, 720),
                  scale_factor=16,
-                 mean=[0.485, 0.456, 0.406],
-                 std=[0.229, 0.224, 0.225]):
+                 mean=[0.485, 0.456, 0.406], 
+                 std=[0.229, 0.224, 0.225],
+                 bd_dilate_size=4):
+
+        super(BDD, self).__init__(ignore_label, base_size,crop_size, scale_factor, mean, std)
+
+        self.root = root
+        self.list_path = list_path
+        self.num_classes = num_classes
+
+        self.multi_scale = multi_scale
+        self.flip = flip
         
-        self.base_size = base_size
-        self.crop_size = crop_size
+        self.img_list = [line.strip().split() for line in open(root+list_path)]
+
+        self.files = self.read_files()
+
+
         self.ignore_label = ignore_label
-
-        self.mean = mean
-        self.std = std
-        self.scale_factor = scale_factor
-
-        self.files = []
-
-    def __len__(self):
-        return len(self.files)
-
-    def input_transform(self, image, city=True):
-        if city:
-            image = image.astype(np.float32)[:, :, ::-1]
-        else:
-            image = image.astype(np.float32)
-        image = image / 255.0
-        image -= self.mean
-        image /= self.std
-        return image
-
-    def label_transform(self, label):
-        return np.array(label).astype(np.uint8)
-
-    def pad_image(self, image, h, w, size, padvalue):
-        pad_image = image.copy()
-        pad_h = max(size[0] - h, 0)
-        pad_w = max(size[1] - w, 0)
-        if pad_h > 0 or pad_w > 0:
-            pad_image = cv2.copyMakeBorder(image, 0, pad_h, 0,
-                                           pad_w, cv2.BORDER_CONSTANT,
-                                           value=padvalue)
-
-        return pad_image
-
-    def rand_crop(self, image, label):
-        h, w = image.shape[:-1]
-        image = self.pad_image(image, h, w, self.crop_size,
-                               (0.0, 0.0, 0.0))
-        label = self.pad_image(label, h, w, self.crop_size,
-                               (self.ignore_label,))
-#         edge = self.pad_image(edge, h, w, self.crop_size,
-#                                (0.0,))
-
-        new_h, new_w = label.shape
-        x = random.randint(0, new_w - self.crop_size[1])
-        y = random.randint(0, new_h - self.crop_size[0])
-        image = image[y:y+self.crop_size[0], x:x+self.crop_size[1]]
-        label = label[y:y+self.crop_size[0], x:x+self.crop_size[1]]
-#         edge = edge[y:y+self.crop_size[0], x:x+self.crop_size[1]]
-
-        return image, label
-
-    def multi_scale_aug(self, image, label=None,
-                        rand_scale=1, rand_crop=True):
-        long_size = np.int(self.base_size * rand_scale + 0.5)
-        h, w = image.shape[:2]
-        if h > w:
-            new_h = long_size
-            new_w = np.int(w * long_size / h + 0.5)
-        else:
-            new_w = long_size
-            new_h = np.int(h * long_size / w + 0.5)
-
-        image = cv2.resize(image, (new_w, new_h),
-                           interpolation=cv2.INTER_LINEAR)
-        if label is not None:
-            label = cv2.resize(label, (new_w, new_h),
-                               interpolation=cv2.INTER_NEAREST)
-#             if edge is not None:
-#                 edge = cv2.resize(edge, (new_w, new_h),
-#                                    interpolation=cv2.INTER_NEAREST)
-        else:
-            return image
-
-        if rand_crop:
-            image, label= self.rand_crop(image, label)
-
-        return image, label
-
-
-
-    def resize_short_length(self, image, label=None, short_length=None, fit_stride=None, return_padding=False):
-        h, w = image.shape[:2]
-        if h < w:
-            new_h = short_length
-            new_w = np.int(w * short_length / h + 0.5)
-        else:
-            new_w = short_length
-            new_h = np.int(h * short_length / w + 0.5)        
-        image = cv2.resize(image, (new_w, new_h),
-                           interpolation=cv2.INTER_LINEAR)
-        pad_w, pad_h = 0, 0
-        if fit_stride is not None:
-            pad_w = 0 if (new_w % fit_stride == 0) else fit_stride - (new_w % fit_stride)
-            pad_h = 0 if (new_h % fit_stride == 0) else fit_stride - (new_h % fit_stride)
-            image = cv2.copyMakeBorder(
-                image, 0, pad_h, 0, pad_w, 
-                cv2.BORDER_CONSTANT, value=tuple(x * 255 for x in self.mean[::-1])
-            )
-
-        if label is not None:
-            label = cv2.resize(
-                label, (new_w, new_h),
-                interpolation=cv2.INTER_NEAREST)
-            if pad_h > 0 or pad_w > 0:
-                label = cv2.copyMakeBorder(
-                    label, 0, pad_h, 0, pad_w, 
-                    cv2.BORDER_CONSTANT, value=self.ignore_label
-                )
-            if return_padding:
-                return image, label, (pad_h, pad_w)
-            else:
-                return image, label
-        else:
-            if return_padding:
-                return image, (pad_h, pad_w)
-            else:
-                return image  
+        
+        self.color_list = [ [128, 64, 128], [244, 35, 232], [70, 70, 70], [102, 102, 156], [190, 153, 153], [153, 153, 153],
+                            [250, 170, 30], [220, 220, 0], [107, 142, 35], [152, 251, 152],  [70, 130, 180],  [220, 20, 60],
+                            [255, 0, 0], [0, 0, 142], [ 0, 0, 70], [0, 60, 100], [0, 80, 100],  [0, 0, 230],
+                            [119, 11, 32]]
+        
+#         palette = [128, 64, 128, 244, 35, 232, 70, 70, 70, 102, 102, 156, 190, 153, 153,
+#            153, 153, 153, 250, 170, 30,
+#            220, 220, 0, 107, 142, 35, 152, 251, 152, 70, 130, 180, 220, 20, 60,
+#            255, 0, 0, 0, 0, 142, 0, 0, 70,
+#            0, 60, 100, 0, 80, 100, 0, 0, 230, 119, 11, 32]
+        self.class_weights = None
+        
+        self.bd_dilate_size = bd_dilate_size
     
+    def read_files(self):
+        files = []
+
+        for item in self.img_list:
+            image_path, label_path = item
+            name = os.path.splitext(os.path.basename(label_path))[0]
+            files.append({
+                "img": image_path,
+                "label": label_path,
+                "name": name
+            })
+            
+        return files
+        
+    def color2label(self, color_map):
+        label = np.ones(color_map.shape[:2])*self.ignore_label
+        for i, v in enumerate(self.color_list):
+            label[(color_map == v).sum(2)==3] = i
+
+        return label.astype(np.uint8)
     
-    def gen_sample(self, image, label,
-                   multi_scale=True, is_flip=True, edge_pad=True, edge_size=4, city=True):
-        
+    def label2color(self, label):
+        color_map = np.zeros(label.shape+(3,))
+        for i, v in enumerate(self.color_list):
+            color_map[label==i] = self.color_list[i]            
+        return color_map.astype(np.uint8)
+
+    def __getitem__(self, index):
+        item = self.files[index]
+        name = item["name"]
+        image = Image.open(os.path.join(self.root,'bdd',item["img"])).convert('RGB')
+
+        image = np.array(image)
+        size = image.shape
+        color_map = Image.open(os.path.join(self.root,'bdd',item["label"])).convert('RGB')
+        color_map = np.array(color_map)
+        label = self.color2label(color_map)
+
+        image, label = self.gen_sample(image, label, self.multi_scale, self.flip, edge_pad=False,edge_size=self.bd_dilate_size, city=False)
+
+        return image.copy(), label.copy(),  np.array(size), name
+
+    def single_scale_inference(self, config, model, image):
+        pred = self.inference(config, model, image)
+        return pred
+
+    def save_pred(self, preds, sv_path, name):
+        preds = np.asarray(np.argmax(preds.cpu(), axis=1), dtype=np.uint8)
+        for i in range(preds.shape[0]):
+            pred = self.label2color(preds[i])
+            save_img = Image.fromarray(pred)
+            save_img.save(os.path.join(sv_path, name[i]+'.png'))
 
         
-        if multi_scale:
-            rand_scale = 0.4 + random.randint(0, self.scale_factor) / 10.0
-
-            image, label = self.multi_scale_aug(image, label,
-                                                rand_scale=rand_scale)
-
-        image = self.input_transform(image, city=city)
-        label = self.label_transform(label)
         
-
-        image = image.transpose((2, 0, 1))
-
-        if is_flip:
-            flip = np.random.choice(2) * 2 - 1
-            image = image[:, :, ::flip]
-            label = label[:, ::flip]
-
-        return image, label
-
-
-    def inference(self, config, model, image):
-        size = image.size()
-        pred = model(image)
-
-        if config.MODEL.NUM_OUTPUTS > 1:
-            pred = pred[config.TEST.OUTPUT_INDEX]
-        
-        
-        pred = F.interpolate(
-            input=pred, size=size[-2:],
-            mode='bilinear', align_corners=config.MODEL.ALIGN_CORNERS
-        )
-        
-        
-        return pred.exp()
-
-    def reduce_zero_label(self, labelmap):
-        labelmap = np.array(labelmap)
-        encoded_labelmap = labelmap - 1
-
-        return encoded_labelmap
