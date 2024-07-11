db90f3a232b95d6dd7525ed4d86567bc3da298cf
takaniwa <52619499+takaniwa@users.noreply.github.com>
2024-07-11 09:44:48 +0800

    Delete models/.ipynb_checkpoints directory

--- models/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,8 +0,0 @@
-# ------------------------------------------------------------------------------
-# Written by Jiacong Xu (jiacong.xu@tamu.edu)
-# ------------------------------------------------------------------------------
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-import models.dsnet
