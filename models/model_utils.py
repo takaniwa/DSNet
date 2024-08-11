7ed40b063bc56c0f6f108a31edfb607d0adafba5
takaniwa <52619499+takaniwa@users.noreply.github.com>
2024-08-11 21:20:29 +0800

    add MSA

--- models/model_utils.py
+++ models/model_utils.py
@@ -353,6 +353,7 @@ class AFF(nn.Module):
         xo = 2 * x * wei + 2 * residual * (1 - wei)
         return xo
 
+# MSAF
 class Muti_AFF(nn.Module):
     '''
     多特征融合 AFF, 一个像素级尺度，多个语义级尺度
@@ -426,6 +427,235 @@ class Muti_AFF(nn.Module):
         wei = self.sigmoid(xlg)
 
         xo = 2 * x * wei + 2 * residual * (1 - wei)
+        return xo
+
+
+# MSAF_small
+class MSAF_small(nn.Module):
+    '''
+    多特征融合 AFF, 一个像素级尺度，多个语义级尺度
+    '''
+    def __init__(self, channels=64, r=4):
+        super(MSAF_small, self).__init__()
+        inter_channels = int(channels // r)
+
+        self.local_att = nn.Sequential(
+            nn.Conv2d(channels, inter_channels, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(inter_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(inter_channels, channels, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(channels),
+        )
+
+        self.context1 = nn.Sequential(
+            nn.AdaptiveAvgPool2d((4, 4)),
+            nn.Conv2d(channels, inter_channels, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(inter_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(inter_channels, channels, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(channels)
+        )
+
+        self.context2 = nn.Sequential(
+            nn.AdaptiveAvgPool2d((8, 8)),
+            nn.Conv2d(channels, inter_channels, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(inter_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(inter_channels, channels, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(channels)
+        )
+
+        # self.context3 = nn.Sequential(
+        #     nn.AdaptiveAvgPool2d((16, 16)),
+        #     nn.Conv2d(channels, inter_channels, kernel_size=1, stride=1, padding=0),
+        #     nn.BatchNorm2d(inter_channels),
+        #     nn.ReLU(inplace=True),
+        #     nn.Conv2d(inter_channels, channels, kernel_size=1, stride=1, padding=0),
+        #     nn.BatchNorm2d(channels)
+        # )
+
+        self.global_att = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(channels, inter_channels, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(inter_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(inter_channels, channels, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(channels),
+        )
+
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x, residual):
+        h, w = x.shape[2], x.shape[3]  # 获取输入 x 的高度和宽度
+
+        xa = x + residual
+        xl = self.local_att(xa)
+        c1 = self.context1(xa)
+        c2 = self.context2(xa)
+        # c3 = self.context3(xa)
+        xg = self.global_att(xa)
+
+        # 将 c1, c2, c3 还原到原本的大小，按均匀分布
+        c1 = F.interpolate(c1, size=[h, w], mode='nearest')
+        c2 = F.interpolate(c2, size=[h, w], mode='nearest')
+        # c3 = F.interpolate(c3, size=[h, w], mode='nearest')
+
+        xlg = xl + xg + c1 + c2 
+        wei = self.sigmoid(xlg)
+
+        xo = 2 * x * wei + 2 * residual * (1 - wei)
+        return xo
+        
+# MSA
+class MSA(nn.Module):
+    '''
+    多特征融合 AFF, 一个像素级尺度，多个语义级尺度
+    '''
+    def __init__(self, channels=64, r=4):
+        super(MSA, self).__init__()
+        inter_channels = int(channels // r)
+
+        self.local_att = nn.Sequential(
+            nn.Conv2d(channels, inter_channels, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(inter_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(inter_channels, channels, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(channels),
+        )
+
+        self.context1 = nn.Sequential(
+            nn.AdaptiveAvgPool2d((4, 4)),
+            nn.Conv2d(channels, inter_channels, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(inter_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(inter_channels, channels, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(channels)
+        )
+
+        self.context2 = nn.Sequential(
+            nn.AdaptiveAvgPool2d((8, 8)),
+            nn.Conv2d(channels, inter_channels, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(inter_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(inter_channels, channels, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(channels)
+        )
+
+        self.context3 = nn.Sequential(
+            nn.AdaptiveAvgPool2d((16, 16)),
+            nn.Conv2d(channels, inter_channels, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(inter_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(inter_channels, channels, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(channels)
+        )
+
+        self.global_att = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(channels, inter_channels, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(inter_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(inter_channels, channels, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(channels),
+        )
+
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        h, w = x.shape[2], x.shape[3]  # 获取输入 x 的高度和宽度
+
+        xa = x 
+        xl = self.local_att(xa)
+        c1 = self.context1(xa)
+        c2 = self.context2(xa)
+        c3 = self.context3(xa)
+        xg = self.global_att(xa)
+
+        # 将 c1, c2, c3 还原到原本的大小，按均匀分布
+        c1 = F.interpolate(c1, size=[h, w], mode='nearest')
+        c2 = F.interpolate(c2, size=[h, w], mode='nearest')
+        c3 = F.interpolate(c3, size=[h, w], mode='nearest')
+
+        xlg = xl + xg + c1 + c2 + c3
+        wei = self.sigmoid(xlg)
+
+        xo = 2 * x * wei
+        return xo
+
+# MSA_small
+class MSA_small(nn.Module):
+    '''
+    多特征融合 AFF, 一个像素级尺度，多个语义级尺度
+    '''
+    def __init__(self, channels=64, r=4):
+        super(MSA_small, self).__init__()
+        inter_channels = int(channels // r)
+
+        self.local_att = nn.Sequential(
+            nn.Conv2d(channels, inter_channels, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(inter_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(inter_channels, channels, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(channels),
+        )
+
+        self.context1 = nn.Sequential(
+            nn.AdaptiveAvgPool2d((4, 4)),
+            nn.Conv2d(channels, inter_channels, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(inter_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(inter_channels, channels, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(channels)
+        )
+
+        self.context2 = nn.Sequential(
+            nn.AdaptiveAvgPool2d((8, 8)),
+            nn.Conv2d(channels, inter_channels, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(inter_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(inter_channels, channels, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(channels)
+        )
+
+        # self.context3 = nn.Sequential(
+        #     nn.AdaptiveAvgPool2d((16, 16)),
+        #     nn.Conv2d(channels, inter_channels, kernel_size=1, stride=1, padding=0),
+        #     nn.BatchNorm2d(inter_channels),
+        #     nn.ReLU(inplace=True),
+        #     nn.Conv2d(inter_channels, channels, kernel_size=1, stride=1, padding=0),
+        #     nn.BatchNorm2d(channels)
+        # )
+
+        self.global_att = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(channels, inter_channels, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(inter_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(inter_channels, channels, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(channels),
+        )
+
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        h, w = x.shape[2], x.shape[3]  # 获取输入 x 的高度和宽度
+
+        xa = x 
+        xl = self.local_att(xa)
+        c1 = self.context1(xa)
+        c2 = self.context2(xa)
+        # c3 = self.context3(xa)
+        xg = self.global_att(xa)
+
+        # 将 c1, c2, c3 还原到原本的大小，按均匀分布
+        c1 = F.interpolate(c1, size=[h, w], mode='nearest')
+        c2 = F.interpolate(c2, size=[h, w], mode='nearest')
+        # c3 = F.interpolate(c3, size=[h, w], mode='nearest')
+
+        xlg = xl + xg + c1 + c2 
+        wei = self.sigmoid(xlg)
+
+        xo = 2 * x * wei
         return xo
 
 class iAFF(nn.Module):
